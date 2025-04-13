5ca3691e6bc88264d53e3816351f6fbc6c60c6f6
madou9 <issoufou.mamadou@gmail.com>
2025-04-13 15:50:53 +0200

    Turned the program into http

--- Switch.go
+++ Switch.go
@@ -5,10 +5,16 @@ import (
 	"net/http"
 )
 
+var short = "Watch Go course"
+var full = "Watch Golang Full course"
+
+var taskItems = []string{short, full}
+
 func main() {
 	fmt.Println("#### Welcome to our TodoList App! ###")
 
 	http.HandleFunc("/", helloUser)
+	http.HandleFunc("/show-tasks", showTasks)
 
 	http.ListenAndServe(":3000", nil)
 
@@ -19,15 +25,9 @@ func helloUser(writer http.ResponseWriter, request *http.Request) {
 
 	fmt.Fprintln(writer, greeting)
 }
+func showTasks(writer http.ResponseWriter, request *http.Request) {
 
-// func printTasks(taskItems []string) {
-// 	fmt.Println("List of my Todos")
-// 	for index, task := range taskItems {
-// 		fmt.Printf("%d: %s\n", index+1, task)
-// 	}
-// }
-
-// func addTask(taskItems []string, newTask string) []string {
-// 	var updatedTaskItems = append(taskItems, newTask)
-// 	return updatedTaskItems
-// }
+	for _, task := range taskItems {
+		fmt.Fprintln(writer, task)
+	}
+}
