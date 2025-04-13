68d777c8cda52c977a10e8a24da44b8a2357213f
madou9 <issoufou.mamadou@gmail.com>
2025-04-13 15:41:02 +0200

    implemented the http in Go

--- Switch.go
+++ Switch.go
@@ -1,32 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("#### Welcome to our TodoList App! ###")
 
-	var Short = "Watch Go crash course"
-	var full = "Watch Golang Full course"
+	http.HandleFunc("/", helloUser)
 
-	var taskItems = []string{Short, full}
+	http.ListenAndServe(":3000", nil)
 
-	printTasks(taskItems)
-	fmt.Println()
-	taskItems = addTask(taskItems, "Go for a run")
-	taskItems = addTask(taskItems, "Practice coding in Go")
-
-	fmt.Println("updated list")
-	printTasks(taskItems)
 }
 
-func printTasks(taskItems []string) {
-	fmt.Println("List of my Todos")
-	for index, task := range taskItems {
-		fmt.Printf("%d: %s\n", index+1, task)
-	}
+func helloUser(writer http.ResponseWriter, request *http.Request) {
+	var greeting = "Hello user. Welcome to our TodoList App!"
+
+	fmt.Fprintln(writer, greeting)
 }
 
-func addTask(taskItems []string, newTask string) []string {
-	var updatedTaskItems = append(taskItems, newTask)
-	return updatedTaskItems
-}
+// func printTasks(taskItems []string) {
+// 	fmt.Println("List of my Todos")
+// 	for index, task := range taskItems {
+// 		fmt.Printf("%d: %s\n", index+1, task)
+// 	}
+// }
+
+// func addTask(taskItems []string, newTask string) []string {
+// 	var updatedTaskItems = append(taskItems, newTask)
+// 	return updatedTaskItems
+// }
